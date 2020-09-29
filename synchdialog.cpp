fa2e202c1ef3a37ec4937bd7e187592936352a1b
Jozef Mlich <imlich@fit.vutbr.cz>
2020-09-29 21:08:30 +0200

    remove timezones from translations

--- synchdialog.cpp
+++ synchdialog.cpp
@@ -47,51 +47,51 @@ SynchDialog::SynchDialog(QList<GpsRoute*> *gpsR,QWidget *mw,ChangeDateTime *chan
 
     showGpsRoutes();
 
-    ui->comboBox_zones->addItem(tr("UTC-12:00 - Baker Island, Howland Island"), 12);
-    ui->comboBox_zones->addItem(tr("UTC-11:00 - Samoa, American Samoa"), 11);
-    ui->comboBox_zones->addItem(tr("UTC-10:00 - Hawaii, Papeete"), 10);
-    ui->comboBox_zones->addItem(tr("UTC-9:00  - Marquesas Islands"), 9.5);
-    ui->comboBox_zones->addItem(tr("UTC-9:00  - Anchorage, Fairbanks, Juneau"), 9);
-    ui->comboBox_zones->addItem(tr("UTC-8:00  - Vancouver, Washington (state), Portland, Las Vegas, California, Baja California"), 8);
-    ui->comboBox_zones->addItem(tr("UTC-7:00  - Alberta, Colorado, Arizona, Chihuahua, Sonora"), 7);
-    ui->comboBox_zones->addItem(tr("UTC-6:00  - Chicago, Costa Rica, Dallas, El Salvador, Guatemala, Honduras, Houston, Manitoba, Mexico City, Nicaragua, Saskatchewan"), 6);
-    ui->comboBox_zones->addItem(tr("UTC-5:00  - Ottawa, Toronto, Montreal, Boston, New York, North Carolina, Washington D.C., Georgia, Miami, Cuba, Jamaica, Haiti, Panama, Colombia, Continental Ecuador, Peru"), 5);
-    ui->comboBox_zones->addItem(tr("UTC-4:30  - Venezuela"), 4.5);
-    ui->comboBox_zones->addItem(tr("UTC-4:00  - Nova Scotia, Dominican Republic, Puerto Rico, Trinidad and Tobago, Amazonas, Bolivia, Continental Chile, Paraguay, San Luis Province"), 4);
-    ui->comboBox_zones->addItem(tr("UTC-3:30  - Newfoundland"), 3.5);
-    ui->comboBox_zones->addItem(tr("UTC-3:00  - Rio de Janeiro, S?o Paulo, Argentina (except San Luis Province), Uruguay, Nuuk"), 3);
-    ui->comboBox_zones->addItem(tr("UTC-2:00  - Fernando de Noronha, South Georgia and the South Sandwich Islands"), 2);
-    ui->comboBox_zones->addItem(tr("UTC-1:00  - Azores, Cape Verde"), 1);
-    ui->comboBox_zones->addItem(tr("UTC-0:00  - Iceland, Faroe Islands, United Kingdom, Ireland, Continental Portugal, Madeira, Morocco, Senegal, Ghana, Côte d'Ivoire"), 0);
-    ui->comboBox_zones->addItem(tr("UTC+1:00  - Albania, Slovenia, Macedonia, Norway, Sweden, Denmark, "
+    ui->comboBox_zones->addItem(("UTC-12:00 - Baker Island, Howland Island"), 12);
+    ui->comboBox_zones->addItem(("UTC-11:00 - Samoa, American Samoa"), 11);
+    ui->comboBox_zones->addItem(("UTC-10:00 - Hawaii, Papeete"), 10);
+    ui->comboBox_zones->addItem(("UTC-9:00  - Marquesas Islands"), 9.5);
+    ui->comboBox_zones->addItem(("UTC-9:00  - Anchorage, Fairbanks, Juneau"), 9);
+    ui->comboBox_zones->addItem(("UTC-8:00  - Vancouver, Washington (state), Portland, Las Vegas, California, Baja California"), 8);
+    ui->comboBox_zones->addItem(("UTC-7:00  - Alberta, Colorado, Arizona, Chihuahua, Sonora"), 7);
+    ui->comboBox_zones->addItem(("UTC-6:00  - Chicago, Costa Rica, Dallas, El Salvador, Guatemala, Honduras, Houston, Manitoba, Mexico City, Nicaragua, Saskatchewan"), 6);
+    ui->comboBox_zones->addItem(("UTC-5:00  - Ottawa, Toronto, Montreal, Boston, New York, North Carolina, Washington D.C., Georgia, Miami, Cuba, Jamaica, Haiti, Panama, Colombia, Continental Ecuador, Peru"), 5);
+    ui->comboBox_zones->addItem(("UTC-4:30  - Venezuela"), 4.5);
+    ui->comboBox_zones->addItem(("UTC-4:00  - Nova Scotia, Dominican Republic, Puerto Rico, Trinidad and Tobago, Amazonas, Bolivia, Continental Chile, Paraguay, San Luis Province"), 4);
+    ui->comboBox_zones->addItem(("UTC-3:30  - Newfoundland"), 3.5);
+    ui->comboBox_zones->addItem(("UTC-3:00  - Rio de Janeiro, S?o Paulo, Argentina (except San Luis Province), Uruguay, Nuuk"), 3);
+    ui->comboBox_zones->addItem(("UTC-2:00  - Fernando de Noronha, South Georgia and the South Sandwich Islands"), 2);
+    ui->comboBox_zones->addItem(("UTC-1:00  - Azores, Cape Verde"), 1);
+    ui->comboBox_zones->addItem(("UTC-0:00  - Iceland, Faroe Islands, United Kingdom, Ireland, Continental Portugal, Madeira, Morocco, Senegal, Ghana, Côte d'Ivoire"), 0);
+    ui->comboBox_zones->addItem(("UTC+1:00  - Albania, Slovenia, Macedonia, Norway, Sweden, Denmark, "
                                    "Germany, the Netherlands, Belgium, Metropolitan France, Switzerland, Austria, Poland, "
                                    "Czech Republic, Slovakia, Hungary, Continental Spain, Italy, Croatia, Serbia, "
                                    "Kosovo, Bosnia and Herzegovina, Tunisia, Algeria, Nigeria, Cameroon, Angola, Kinshasa"), -1);
-    ui->comboBox_zones->addItem(tr("UTC+2:00  - Finland, Lithuania, Latvia, Estonia, Belarus, Ukraine, Romania, Bulgaria, "
+    ui->comboBox_zones->addItem(("UTC+2:00  - Finland, Lithuania, Latvia, Estonia, Belarus, Ukraine, Romania, Bulgaria, "
                                    "Greece, Turkey, Cyprus, Syria, Lebanon, Jordan, Palestine, Israel, Egypt, Libya, Mozambique, "
                                    "Malawi, Zambia, Zimbabwe, South Africa"), -2);
-    ui->comboBox_zones->addItem(tr("UTC+3:00  - Samara, Iraq, Saudi Arabia, Yemen, Sudan, Ethiopia, Somalia, Kenya, Uganda, Tanzania, Madagascar"), -3);
-    ui->comboBox_zones->addItem(tr("UTC+3:30  - Iran"), -3.5);
-    ui->comboBox_zones->addItem(tr("UTC+4:00  - Georgia, Armenia, Azerbaijan, United Arab Emirates, Oman, Seychelles, Mauritius, Moscow, Saint Petersburg"), -4);
-    ui->comboBox_zones->addItem(tr("UTC+4:30  - Afghanistan"), -4.5);
-    ui->comboBox_zones->addItem(tr("UTC+5:00  - Sverdlovsk, Uzbekistan, Pakistan, Maldives, Kazakhstan"), -5);
-    ui->comboBox_zones->addItem(tr("UTC+5:30  - India, Sri Lanka"), -5.5);
-    ui->comboBox_zones->addItem(tr("UTC+5:45  - Nepal"), -5.75);
-    ui->comboBox_zones->addItem(tr("UTC+6:00  - Novosibirsk, Almaty, Bangladesh"), -6);
-    ui->comboBox_zones->addItem(tr("UTC+6:30  - Myanmar, Cocos Islands"), -6.5);
-    ui->comboBox_zones->addItem(tr("UTC+7:00  - Krasnoyarsk, Thailand, Vietnam, Jakarta"), -7);
-    ui->comboBox_zones->addItem(tr("UTC+7:30  - Java time"), -7.5);
-    ui->comboBox_zones->addItem(tr("UTC+8:00  - Irkutsk, Ulan Bator, China, Taiwan, Hong Kong, Philippines, Malaysia, Singapore, Western Australia"), -8);
-    ui->comboBox_zones->addItem(tr("UTC+9:00  - Zabaykalsky, Japan, North Korea, South Korea, East Timor"), -9);
-    ui->comboBox_zones->addItem(tr("UTC+9:30  - Northern Territory, South Australia"), -9.5);
-    ui->comboBox_zones->addItem(tr("UTC+10:00 - Victoria, Tasmania, Queensland, New South Wales, Primorsky"), -10);
-    ui->comboBox_zones->addItem(tr("UTC+10:30 - Lord Howe Island"), -10.5);
-    ui->comboBox_zones->addItem(tr("UTC+11:00 - Kamchatka, Solomon Islands, New Caledonia"), -11);
-    ui->comboBox_zones->addItem(tr("UTC+11:30 - Norfolk Island"), -11.5);
-    ui->comboBox_zones->addItem(tr("UTC+12:00 - Fiji, New Zealand"), -12);
-    ui->comboBox_zones->addItem(tr("UTC+12:45 - Chatham Islands"), -12.45);
-    ui->comboBox_zones->addItem(tr("UTC+13:00 - Tonga"), -13);
-    ui->comboBox_zones->addItem(tr("UTC+14:00 - Line Islands"), -14);
+    ui->comboBox_zones->addItem(("UTC+3:00  - Samara, Iraq, Saudi Arabia, Yemen, Sudan, Ethiopia, Somalia, Kenya, Uganda, Tanzania, Madagascar"), -3);
+    ui->comboBox_zones->addItem(("UTC+3:30  - Iran"), -3.5);
+    ui->comboBox_zones->addItem(("UTC+4:00  - Georgia, Armenia, Azerbaijan, United Arab Emirates, Oman, Seychelles, Mauritius, Moscow, Saint Petersburg"), -4);
+    ui->comboBox_zones->addItem(("UTC+4:30  - Afghanistan"), -4.5);
+    ui->comboBox_zones->addItem(("UTC+5:00  - Sverdlovsk, Uzbekistan, Pakistan, Maldives, Kazakhstan"), -5);
+    ui->comboBox_zones->addItem(("UTC+5:30  - India, Sri Lanka"), -5.5);
+    ui->comboBox_zones->addItem(("UTC+5:45  - Nepal"), -5.75);
+    ui->comboBox_zones->addItem(("UTC+6:00  - Novosibirsk, Almaty, Bangladesh"), -6);
+    ui->comboBox_zones->addItem(("UTC+6:30  - Myanmar, Cocos Islands"), -6.5);
+    ui->comboBox_zones->addItem(("UTC+7:00  - Krasnoyarsk, Thailand, Vietnam, Jakarta"), -7);
+    ui->comboBox_zones->addItem(("UTC+7:30  - Java time"), -7.5);
+    ui->comboBox_zones->addItem(("UTC+8:00  - Irkutsk, Ulan Bator, China, Taiwan, Hong Kong, Philippines, Malaysia, Singapore, Western Australia"), -8);
+    ui->comboBox_zones->addItem(("UTC+9:00  - Zabaykalsky, Japan, North Korea, South Korea, East Timor"), -9);
+    ui->comboBox_zones->addItem(("UTC+9:30  - Northern Territory, South Australia"), -9.5);
+    ui->comboBox_zones->addItem(("UTC+10:00 - Victoria, Tasmania, Queensland, New South Wales, Primorsky"), -10);
+    ui->comboBox_zones->addItem(("UTC+10:30 - Lord Howe Island"), -10.5);
+    ui->comboBox_zones->addItem(("UTC+11:00 - Kamchatka, Solomon Islands, New Caledonia"), -11);
+    ui->comboBox_zones->addItem(("UTC+11:30 - Norfolk Island"), -11.5);
+    ui->comboBox_zones->addItem(("UTC+12:00 - Fiji, New Zealand"), -12);
+    ui->comboBox_zones->addItem(("UTC+12:45 - Chatham Islands"), -12.45);
+    ui->comboBox_zones->addItem(("UTC+13:00 - Tonga"), -13);
+    ui->comboBox_zones->addItem(("UTC+14:00 - Line Islands"), -14);
 
     tzset();
 #ifdef _XOPEN_SOURCE
