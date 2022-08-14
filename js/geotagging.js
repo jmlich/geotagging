5dafcd52a03c005469e0820df052a74676af16e2
Jozef Mlich <jmlich83@gmail.com>
2022-08-14 11:10:11 +0200

    paint direction into map

--- js/geotagging.js
+++ js/geotagging.js
@@ -387,10 +387,13 @@ function setNewCameraDirection(id_list, isVisible, direction) {
         for (var i in aovMarkers) {
             if (aovMarkers[i].options.id === id) {
                 angle_of_view = aovMarkers[i].options.angle_of_view
+                var A = getCoordByDistanceBearing( position, direction-angle_of_view/2, 1000);
+                var B = getCoordByDistanceBearing( position, direction+angle_of_view/2, 1000);
+                console.log ("Updating angle of view: " + position + " " + direction + " " +angle_of_view)
+                aovMarkers[i].setLatLngs([A, position, B])
+
             }
         }
-
-        addCameraMarker(id, isVisible, position[0], position[1], direction, angle_of_view)
 
     }
 
